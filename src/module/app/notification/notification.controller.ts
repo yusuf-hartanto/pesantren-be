3a6fa6a4b65b13ce4e6a965bd05fdd84ecbd6454
Samsul Anwar <sam.aan46@gmail.com>
2026-06-09 23:06:36 +0700

    module notification

---
+++ src/module/app/notification/notification.controller.ts
@@ -0,0 +1,90 @@
+'use strict';
+
+import { Request, Response } from 'express';
+import { helper } from '../../../helpers/helper';
+import { variable } from './notification.variable';
+import { response } from '../../../helpers/response';
+import { repository } from './notification.repository';
+import {
+  ALREADY_EXIST,
+  NOT_FOUND,
+  SUCCESS_DELETED,
+  SUCCESS_RETRIEVED,
+  SUCCESS_SAVED,
+  SUCCESS_UPDATED,
+} from '../../../utils/constant';
+import moment from 'moment';
+
+const date: string = helper.date();
+
+export default class Controller {
+
+  public async index(req: Request, res: Response) {
+    try {
+      const query = helper.fetchQueryRequest(req);
+      const { count, rows } = await repository.index(query);
+      if (rows?.length < 1)
+        return response.success(NOT_FOUND, null, res, false);
+      return response.success(
+        SUCCESS_RETRIEVED,
+        { total: count, values: rows },
+        res
+      );
+    } catch (err: any) {
+      return helper.catchError(
+        `notifications index: ${err?.message}`,
+        500,
+        res
+      );
+    }
+  }
+
+  public async update(req: Request, res: Response) {
+    try {
+      const id: string = req?.params?.id || '';
+
+      const { status } = req?.body;
+      const check = await repository.detail({ id_notification: id });
+      if (!check) return response.success(NOT_FOUND, null, res, false);
+
+      const data: Object = helper.only(variable.fillable(), req?.body, true);
+
+      await repository.update({
+        payload: {
+          ...data,
+        },
+        condition: { id_notification: id },
+      });
+
+      return response.success(SUCCESS_UPDATED, null, res);
+    } catch (err: any) {
+      return helper.catchError(
+        `notifications update: ${err?.message}`,
+        500,
+        res
+      );
+    }
+  }
+
+  public async delete(req: Request, res: Response) {
+    try {
+      const id: string = req?.params?.id || '';
+      const check = await repository.detail({ id_notification: id });
+      if (!check) return response.success(NOT_FOUND, null, res, false);
+
+      await repository.delete({
+        condition: { id_notification: id },
+      });
+
+      return response.success(SUCCESS_DELETED, null, res);
+    } catch (err: any) {
+      return helper.catchError(
+        `notifications delete: ${err?.message}`,
+        500,
+        res
+      );
+    }
+  }
+}
+
+export const notification = new Controller();
---
+++ src/module/app/notification/notification.model.ts
@@ -0,0 +1,79 @@
+'use strict';
+
+import { v4 as uuidv4 } from 'uuid';
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import AppResource from '../resource/resource.model';
+
+export class Notification extends Model {
+  declare id_notification: string;
+  declare from: string;
+  declare to: string;
+  declare title: string;
+  declare type: string;
+  declare url: string;
+  declare message: string;
+  declare status: number;
+}
+
+export function initNotification(sequelize: Sequelize) {
+  Notification.init(
+    {
+      id_notification: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true,
+      },
+      from: {
+        type: DataTypes.STRING,
+      },
+      to: {
+        type: DataTypes.STRING,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      type: {
+        type: DataTypes.STRING,
+      },
+      url: {
+        type: DataTypes.STRING,
+      },
+      message: {
+        type: DataTypes.TEXT,
+      },
+      status: {
+        type: DataTypes.SMALLINT,
+        allowNull: true,
+        defaultValue: 0,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Notification',
+      tableName: 'notifications',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
+
+  Notification.beforeCreate((row) => {
+    row?.setDataValue('id_notification', uuidv4());
+  });
+
+  return Notification;
+}
+
+export function associateNotification() {
+  Notification.belongsTo(AppResource, {
+    as: 'from',
+    foreignKey: 'from',
+  });
+
+  Notification.belongsTo(AppResource, {
+    as: 'to',
+    foreignKey: 'to',
+  });
+}
+
+export default Notification;
